385345559ef5f93af63e73d29baa8904bce7453c
Roberto Barros <roberto.f.barros@gmail.com>
2021-08-11 16:27:35 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// API Doc: https://www.last.fm/api/show/artist.getTopAlbums
+
+// http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&api_key=99b6a9d8450ca4686ff1bb85a5c80edc&artist=u2&format=json&limit=5
+
+const api_key = "99b6a9d8450ca4686ff1bb85a5c80edc";
+const baseUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums`
+
